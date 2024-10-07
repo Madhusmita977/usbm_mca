77a296f13e3aaac650d29e912af2816f4365ec05
Madhusmita977 <nayakmadhusmita.nayak2020@gmail.com>
2024-10-07 19:56:36 +0530

    string task added

---
+++ frontened/JAVASCRIPT/class-1/index.js
@@ -0,0 +1,94 @@
+console.log("i am madhusmita");
+
+//var age =25;
+//let name ="madhu";
+//const city ="bbsr";
+console.log(address);
+var address ="bbsr";
+console.log(address);
+
+//null
+//let user;
+
+let user =null;
+console.log(typeof null);
+//console.log(user);
+
+//undefined
+let data;
+console.log(typeof data);
+console.log(data);
+
+//number
+let number = 50;
+let decimal_no = 1.001;
+console.log(typeof num1);
+
+//addition
+let num2 = 100;
+let num3 = 200;
+let addition = num2+num3;
+console.log(addition);
+
+//substraction
+
+let num5 = 200;
+let num4 = 400;
+let substraction = num5-num4;
+console.log(substraction);
+let mul = num5*num4;
+console.log(mul);
+let div = num5/num4;
+console.log(div);
+
+//big int
+let huge_no =
+22222222222222222222222222222222222222222222222222222222222544444444444444;
+console.log(typeof huge_no);
+
+//symbol
+
+let sym1 = Symbol("sym1");
+let sym2 =Symbol("sym2");
+console.log(sym1===sym2); //false
+
+//nan(not a number)
+let num6 = 0;
+let num7 = 0;
+console.log(num6/num7); //nan
+
+//infinite
+
+let num8 = 0;
+let num9 =10;
+console.log(num9/num8);//infinite
+
+//boolean
+
+let ismarried = false;
+let issingle = true;
+console.log(typeof ismarried);
+console.log( issingle);
+//string
+
+let name ="rupa";
+let age =22;
+let city = "bhadrak";
+let college_name = "usbm";
+let roll_no = 57;
+console.log(`my name is: ${name},age is: ${age},and city is:${city},college_name is:${college_name}`)
+
+//to check the length of the string
+
+console.log(city.length);//7
+
+let my_website
+
+
+
+
+
+
+
+
+
